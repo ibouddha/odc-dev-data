825cbbb33c9484cff64a1b24bdf6524118824271
mouhamedbaba <lamottelymouhamed@gmail.com>
2024-05-05 15:22:33 +0000

    fix : rmv fb analitics

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,5 +1,4 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
@@ -23,6 +22,5 @@ export const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
